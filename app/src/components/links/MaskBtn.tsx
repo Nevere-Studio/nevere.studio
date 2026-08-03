aa9d871d5f31faf0199cd23682716591f3ae396c
Cuby92 <jakub.bartul@hotmail.com>
2026-08-03 17:23:35 +0200

    Create MaskBtn

---
+++ app/src/components/links/MaskBtn.tsx
@@ -0,0 +1,40 @@
+import s from './links.module.scss';
+import type { LinkProps } from '@/utils/types';
+import { Link } from '@/i18n/navigation';
+import ArrowRightIcon from '@iconify-react/mdi/arrow-right';
+
+interface Props extends LinkProps {
+    type: 'internal' | 'external'
+}
+
+function MaskBtn(props: Props) {
+    const { type, className, children } = props;
+
+    let typeClass;
+    switch (type) {
+        case 'internal':
+            typeClass = s.internal;
+            break;
+        case 'external':
+            typeClass = s.external;
+            break;
+        default:
+            typeClass = '';
+            break;
+    }
+
+    return (
+        <Link
+            {...props}
+            className={`${s.MaskBtn} ${className || ''}`}
+        >
+            <div className={s.dot} />
+            <div className={s.text}>
+                { children }
+            </div>
+            <ArrowRightIcon className={`${s.arrow} ${typeClass}`} />
+        </Link>
+    )
+}
+
+export default MaskBtn;
